8b64e9c99cf67d6e3998395973baadce21a3dbad
Pm Pranta <heyiampm@gmail.com>
2026-06-10 20:20:31 +0600

    Break & Continue in python

---
+++ Break & Continue/Break continue.py
@@ -0,0 +1,16 @@
+#Python break Keyword.
+
+# table = [1,2,3,4,5,6,7,8,9,10]
+# for y in range(len(table)):
+#           if y == 7:
+#                   break
+#           print("This number is stop",[y])
+
+
+# Python continue keyword
+
+lists = ["pm", "ap", "km", "mm","bp", "Ap"]
+for i in (lists):
+        if i == "mm":
+                continue
+        print(i)
