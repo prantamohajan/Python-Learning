9f96bc74b585939f27e4d01ee7ebc712369f0a6e
Pm Pranta <heyiampm@gmail.com>
2026-06-10 20:19:41 +0600

    Array in python

---
+++ Array/Array.py
@@ -0,0 +1,9 @@
+# Python Arrays
+myarray = ["bmw", "toyota", "volvo", "bike","car"]
+print(type(myarray))
+myarray[0] = "pranta mohajan"
+print(myarray)
+
+# Array Methods()
+# //append().clear()//copy()//count()	
+# extend()//index()//insert()//pop()//remove()//reverse()//sort()
