2c2a2951a8bdc9ee446122a8101531961fb5ad2c
Pm Pranta <heyiampm@gmail.com>
2026-06-11 12:17:53 +0600

    Introduction in python

---
+++ Introduction/Day1.py
@@ -0,0 +1,31 @@
+# # first program
+print("Hello Program")
+print("Any value,object,name,number.")
+
+
+#variable declare.
+
+name = "Pranta Mohajan"
+print(name)
+
+
+
+# #String formating.//new coming {'f'}.always use  befor question.
+
+
+username = "Pranta mohajan."
+address = "Roajan in chittagong."
+print(f"My name is {username}\nMy address is a {address}")
+
+# #Binary type data.[bytes,byte array]/class bytes
+#maximum range is 255.
+
+List = [1,2,3,4,5,6,7,8,9,10,245,255]
+b = bytes(List)
+print(bytes)
+
+# Bytearray//== always use square brackets]/class bytearray
+
+List_1 = [2,33,44,55,66]
+b1 = bytearray(List_1)
+print(bytearray)
---
+++ Introduction/day2.py
@@ -0,0 +1,13 @@
+#Python Casting.//type casting.//("convert").
+#String to integer data type convert.
+
+name = "12345678"
+
+print(int(name))
+# print(type(name))
+print(type(int(name)))
+
+#string to float data type convert.
+
+num = '20'
+print(float(num)) 
