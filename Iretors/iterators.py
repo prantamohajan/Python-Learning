2bc3f6832592a4fc8d9b7e5b3196d1949a044866
Pm Pranta <heyiampm@gmail.com>
2026-06-11 11:33:23 +0600

    Iretors in python

---
+++ Iretors/iterators.py
@@ -0,0 +1,10 @@
+# Iterator vs Iterable
+
+list = [12,34,56,78,"Pranta", "arpita", "toyota","mercedes"]
+x = iter(list)
+
+# print(x.__next__)
+# print(x.__next__)                  #this is object.
+
+
+print(next(x))
