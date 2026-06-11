fc348e81d43febc31a51a4e82ec5aff668bf13ea
Pm Pranta <heyiampm@gmail.com>
2026-06-11 11:30:40 +0600

    Try & Expect in python

---
+++ Try & Expect/Try & Except.py
@@ -0,0 +1,18 @@
+# Python Try Except.
+
+try:
+          print("Your net connection is a very poor")
+
+except:
+          print(x)
+          print("yes network is medium")
+          print("yes network is worked")
+print("yes network is worked")
+print("yes network is worked")
+print("yes network is worked")
+print("yes network is worked")
+print("yes network is worked")
+
+
+
+
